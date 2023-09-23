1222fbd66a4fe72eb550d3adbe4678239e873488
lavanshi295 <107937842+lavanshi295@users.noreply.github.com>
2023-09-23 12:47:13 +0530

    Time: 23 ms (93.59%), Space: 19.3 MB (34.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -7,14 +7,19 @@ public:
         //2. check for overlapping intervals
         vector<vector<int>>ans;
         for(int i=0; i<n; i++){
-            if(ans.empty())
+            if(ans.empty()) //if ans is empty then push intervals in it
             ans.push_back(intervals[i]);
+
             else{
+                //make an vector to store the last interval in our ans
                 vector<int>&v = ans.back(); //end time of previous interval
                 int y = v[1]; 
                 //compare y with i's start time
+                //overlapping intervals
                 if(intervals[i][0] <= y)
                 v[1] = max(intervals[i][1], y);
+
+                //non-overlapping intervals
                 else ans.push_back(intervals[i]);
             }
         }
